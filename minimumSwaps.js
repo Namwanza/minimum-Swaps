b147a7cc1e6560b5e6240ca6d9e3f9b538d2cf04
ronnie-web <namwanza.dev@gmail.com>
2019-05-20 06:16:39 +0300

    Initial commit

---
+++ minimumSwaps.js
@@ -0,0 +1,16 @@
+function minimumSwaps(ratings) {
+    var swap = 0;
+    var visited = [];
+    ratings.forEach(function (rating, i) {
+      var cycle = 0;
+      while(!visited[i]) {
+        visited[i] = true;
+        i = ratings.length - ratings[i];
+        cycle += 1;
+      }
+      if (cycle != 0) {
+        swap += cycle - 1;
+      }
+    });
+    return swap;
+  }
